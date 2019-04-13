f35c8a9665a7a99684662f252e41131115a06f51
junzhiL <43593906+junzhiL@users.noreply.github.com>
2019-04-13 22:53:25 +0800

    querystatistics

---
+++ application/querystatistics/controller/MultiQuery.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 84333
+ * Date: 2019/4/13
+ * Time: 22:50
+ */
+
+namespace app\querystatistics\controller;
+
+
+use app\common\controller\Common;
+
+class MultiQuery extends Common
+{
+    public function index(){
+        return $this->fetch();
+    }
+}
---
+++ application/querystatistics/controller/Query.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 84333
+ * Date: 2019/4/13
+ * Time: 22:47
+ */
+
+namespace app\querystatistics\controller;
+
+
+use app\common\controller\Common;
+
+class Query extends Common
+{
+    public function index(){
+        return $this->fetch();
+    }
+}
