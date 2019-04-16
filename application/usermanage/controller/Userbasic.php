eb0fa6f273423f54ac375fd2f02072db8e6858ee
felix_zhao <zhaobowen9876@gmail.com>
2019-04-16 22:02:48 +0800

    Default Changelist

--- application/usermanage/controller/Userbasic.php
+++ application/usermanage/controller/Userbasic.php
@@ -14,6 +14,38 @@ use app\common\controller\Common;
 class Userbasic extends Common
 {
     public function index(){
+        $userbasic = model('Userbasic');
+        $info = $userbasic->getinfo();
+        foreach($info as $key=>$value){
+            if ($info[$key]['type_id'] == 0){
+                $info[$key]['type'] = '普通用户';
+            }elseif ($info[$key]['type_id'] == 1){
+                $info[$key]['type'] = '院领导';
+            }elseif ($info[$key]['type_id'] == 2){
+                $info[$key]['type'] = '部门领导';
+            }elseif ($info[$key]['type_id'] == 3){
+                $info[$key]['type'] = '系领导';
+            }
+        }
+        $this->assign('info',$info);
+
+        //添加人员模态框传值
+        $depart = $userbasic->getdepart();
+        $this->assign('depart',$depart);
+        $position = $userbasic->getposition();
+        $this->assign('position',$position);
         return $this->fetch();
+    }
+
+    public function edituserinfo()
+    {
+        $userbasic = model('Userbasic');
+        $data = input('post.');
+        $is_add = $userbasic->edituserinfo($data);
+        if ($is_add) {
+            $this->success('修改成功！');
+        } else {
+            $this->error('修改失败！');
+        }
     }
 }
