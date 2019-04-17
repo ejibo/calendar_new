d9c61536a64999d1a4d87fc2adc9ac087b3752f6
quqian <784459130@qq.com>
2019-04-17 15:54:28 +0800

    设置隐私条款v1.0

--- application/msgmanage/controller/Policy.php
+++ application/msgmanage/controller/Policy.php
@@ -11,10 +11,40 @@ namespace app\msgmanage\controller;
 
 use app\common\controller\Common;
 
+function utf8_fopen_read($fileName)
+{
+    $fc = iconv('windows-1250', 'utf-8', file_get_contents($fileName));
+    $handle=fopen("php://memory", "rw");
+    fwrite($handle, $fc);
+    fseek($handle, 0);
+    return $handle;
+}
+
 class Policy extends Common
 {
     public function index(){
-        return $this->fetch();
+        return getProvisionDetail();
     }
 
+    public function getProvisionDetail()
+    {
+        $provisionFile = utf8_fopen_read("provisions.txt", "r") or die("Unable to open file!");
+        $last = fread($provisionFile, filesize("provisions.txt"));
+        fclose($provisionFile);
+        $this->assign('last', $last);
+        return $this->fetch('index');
+    }
+
+    public function postProvisionDetail()
+    {
+        $data = input('textarea-input');
+        $provisionFile = fopen("provisions.txt", "w") or die("Unable to open file!");
+        $tag = fwrite($provisionFile, $data);
+        fclose($provisionFile);
+        if ($tag) {
+            return json(['msg' => '成功！', 'code' => 1]);
+        } else {
+            return json(['msg' => '失败！', 'code' => 0]);
+        }
+    }
 }
