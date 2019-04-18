180270aed1e277377a69578728695129ba4b412b
tong <793825211@qq.com>
2019-04-18 20:58:17 +0800

    message

--- application/msgmanage/controller/Msgmodel.php
+++ application/msgmanage/controller/Msgmodel.php
@@ -14,7 +14,45 @@ use app\common\controller\Common;
 class Msgmodel extends Common
 {
     public function index(){
+        $model = model('Template');
+        $templateItems = $model->getAllTemplates();
+        $this->assign('templateItems',$templateItems);
         return $this->fetch();
+    }
+
+    /*
+     *story:
+     *负责人：
+     */
+    public function loadTemplate()
+    {
+        $template = model('Template');
+        $templates = $template->getAllTemplates();
+        return $templates;
+    }
+
+    /*
+     *story:添加消息模板
+     *负责人：佟起
+     */
+    public function  addTemplate()
+    {
+        $des = $_POST['des'];
+        $con = $_POST['con'];
+        $model = model('Template');
+        $isHasSame = $model->getItemByTitle($des);
+        if ($isHasSame == null) {
+            $res = $model->insertTemplate($des, $con);
+            if($res ==1){
+                $this->success("新增成功");
+            }
+            else{
+                $this->error("添加失败，请重新尝试");
+            }
+        }
+        else{
+            $this->error("名称重复");
+        }
     }
 
 }
---
+++ application/msgmanage/model/Template.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by 佟起.
+ * User: 
+ * Date: 2019/4/18
+ */
+
+namespace app\msgmanage\model;
+use think\Model;
+use think\Db;
+
+class Template extends Model
+{
+    //绑定表名
+    /* protected $table = 'message_template';
+    protected $pk = 'id'; */
+
+    //根据模板名称获取模板
+    public function getItemByTitle($tit){
+        $titleTemp = Db::name('message_template')
+            ->where('name',$tit)
+            ->where('is_delete',0)
+            ->find();
+        return $titleTemp;
+    }
+
+    //获取所有模板
+    public function getAllTemplates(){
+        $allItems = Db::name('message_template')
+            ->where('is_delete',0)
+            ->select();
+        return $allItems;
+    }
+
+    //插入模板
+    public function insertTemplate($tit, $cont){
+        $data = ['title' => $des, content=> $cont, 'is_delete' => 0,'update_time'=> date('Y-m-d H:i:s',time())];
+        $res = Db::name('message_template')->insert($data);
+        return $res;
+    }
+
+}
