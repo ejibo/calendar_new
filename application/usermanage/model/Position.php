197de1fe323dfef26d95ce14c10c4b2f1f70cb24
fataljoy <zhangchuyue0407@gmail.com>
2019-04-18 22:10:39 +0800

    2

--- application/usermanage/model/Position.php
+++ application/usermanage/model/Position.php
@@ -4,17 +4,19 @@ namespace app\usermanage\model;
 use think\Model;
 use think\Db;
 
-class Position extends Model{
-	
- /**
-  * 第05组 高裕欣
-  * 功能：显示列表
-  */
-	public function getUserPositionList(){
-     $list = Db::table('user_position')
-                ->select();
-     return $list;
- }
+class Position extends Model
+{
+
+    /**
+     * 第05组 高裕欣
+     * 功能：显示列表
+     */
+    public function getUserPositionList()
+    {
+        $list = Db::table('user_position')
+            ->select();
+        return $list;
+    }
 
     /**
      *第05组 张君兰
@@ -27,50 +29,56 @@ class Position extends Model{
         $position->name = $name;
         $position->save();//将更新提交至数据库表
     }
- /**
-  * 第05组 高裕欣
-  * 功能：作废职位
-  */
- function invalid($user_id) {
+
+    /**
+     * 第05组 高裕欣
+     * 功能：作废职位
+     */
+    function invalid($user_id)
+    {
         $data = array();
         $data['is_delete'] = 1;
-        $data['delete_time'] =  Db::raw('now()');
+        $data['delete_time'] = Db::raw('now()');
         Db::table('user_position')->where('id', $user_id)->update($data);
     }
 
-function restore($user_id) {
+    function restore($user_id)
+    {
         $data = array();
         $data['is_delete'] = 0;
-        $data['delete_time'] =  Db::raw('now()');
+        $data['delete_time'] = Db::raw('now()');
         Db::table('user_position')->where('id', $user_id)->update($data);
     }
-  /**
- * 第05组 张楚悦
- * 功能：添加职位
- */
-public function add($name)
-{
-    // 接受用户职务信息
-    $status = 1;
-    $message = '用户名可用';
 
-    if (Position::get(['name'=> $name])) {
-        //如果在表中查询到该用户名
-        $status = 0;
-        $message = '职务已存在,请重新输入';
-    }
-    return ['status'=>$status, 'message'=>$message];
-
-    $result = $this -> validate($name);
-
-    if ($result === true) {
-        $position = new Position;
-        $position->name = $name;
-        $position->create_time = time();
-        $position->save();
+    /**
+     * 第05组 张楚悦
+     * 功能：添加职位
+     */
+    public function add($name)
+    {
+        // 接受用户职务信息
         $status = 1;
-        $message = '添加成功';
+        $message = '用户名可用';
+
+        if (Position::get(['name' => $name])) {
+            //如果在表中查询到该用户名
+            $status = 0;
+            $message = '职务已存在,请重新输入';
+        }
+        return ['status' => $status, 'message' => $message];
+
+        $result = $this->validate($name);
+
+        if ($result === true) {
+            $position = new Position;
+            $position->name = $name;
+            $position->create_time = time();
+            $position->save();
+            $status = 1;
+            $message = '添加成功';
+        }
     }
+}
 
 
 
