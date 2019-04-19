5fa62ece623af8543b3c3f082cfe0a926b4d3bc1
juliezhang1112 <1060074157@qq.com>
2019-04-19 13:19:10 +0800

    update index.html

--- application/login/model/mobile.php
+++ application/login/model/mobile.php
@@ -1,14 +1,14 @@
 <?php
-namespace app\login\model;
+
 use think\Model;
 use think\Db;
 
-class mobile extends Model
+class Mobile extends Model
 {
     /**
      * 吴欣雨
      * 功能：将管理员手机号插入到数据库
-     * @param $id ,$mobile
+     * @param $id, $mobile
      * @return int|string
      */
     public function addMobile($id, $mobile)
@@ -20,13 +20,13 @@ class mobile extends Model
     /**
      * 吴欣雨
      * 功能：在管理员数据库中查找手机号，如果手机号存在，则返回手机号，如果手机号不存在，返回false
-     * @param $id
-     * @return string
+     * @param $telephone
+     * @return boolean
      */
-    public function hasMobile($id)
+    public function hasMobile($telephone)
     {
-        $data = Db::name('manage_info')->where('id', $id)->select();//查询对应id管理员的记录
-        if ($data['mobilephone'] == null) return null;
-        else return $data['mobilephone'];
+        $data = Db::name('manage_info')->where('telephone', $telephone)->select();//查询对应管理员的记录
+        if (empty($data)) return false;
+        else return true;
     }
 }
