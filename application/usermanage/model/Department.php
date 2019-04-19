fa8455a5818a23214253fe7464bd145b6b1c4a85
Anastasia-Lu <fcc4523bc0294fa2b70af3b758bd1d88@huawei.com>
2019-04-19 09:05:21 +0800

    注释掉rule

--- application/usermanage/model/Department.php
+++ application/usermanage/model/Department.php
@@ -63,18 +63,18 @@ class Department extends Model
             $message = '部门已存在,请重新输入';
         }
         return ['status'=>$status, 'message'=>$message];
-        $rule = [
-            'name|部门名称' => "require|min:1|max:30",
-        ];
+        #$rule = [
+           # 'name|部门名称' => "require|min:1|max:30",
+      #  ];
 
-        $result = $this -> validate($name, $rule);
+        #$result = $this -> validate($name, $rule);
 
         //if ($result === true) {
         $department = new Department();
         #$department->insert($name);
         $department->name = $name;
         #$department->create_time = time();
-        $department->add();
+        $department->save();
         #$department->save([
              # 'name'  => $name
           ]);
