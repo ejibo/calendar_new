23defaa8caa501af729bfa5a0017f93351f7c575
Rakycc <574db89b19b749558ffa3eefc3a6d7ff@huawei.com>
2019-04-19 14:47:17 +0800

    debug

--- application/login/controller/Smsbinding.php
+++ application/login/controller/Smsbinding.php
@@ -37,7 +37,7 @@ class Smsbinding extends Common
             srand($seed);                     // 播下随机数发生器种子
             $verifyCode = rand(100000, 999999);
             $client = new  ZhenziSmsClient("https://sms_developer.zhenzikj.com", "101241", "7c697169-8031-4c8d-8a5f-653c107e6711");
-            $result = $client->send($_GPC['phone'], "您的验证码为" + $verifyCode + "，有效时间为5分钟");
+            $result = $client->send($telephone, "您的验证码为" + $verifyCode + "，有效时间为5分钟");
             var_dump($result);
             if($result['code']=='0'){
                 $res['code']=1;
