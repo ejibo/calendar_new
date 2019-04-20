47cc234fe09a9a4601c5d5d8c62f56735298b490
icoty <icoty.yangy@gmail.com>
2019-04-20 12:29:19 +0800

    logmanage api

--- application/logmanage/controller/Selflog.php
+++ application/logmanage/controller/Selflog.php
@@ -16,6 +16,29 @@ use app\logmanage\model\Log as LogModel;
 class Selflog extends Common
 {
     public function index(){
+       // dump(request());
+        $model = new LogModel();
+        $model1 = new LogModel();
+
+        $uid = 1;
+        $uid1 = 2;
+        //$agent = Request::instance()->header('user-agent');
+        // $ip = Request()->ip();
+
+        $model->addMangerLog($uid, 1);
+        $model->addMangerLog($uid, 2, 't增加', ['11']);
+        $model->addMangerLog($uid, 3, 't修改', 'aa_'.'bb', 'faa_'.'fbb', 'taa_'.'tbb');
+
+        $model1->addWechatLog($uid1, 1);
+        $model1->addWechatLog($uid1, 2, 't增加', ['11']);
+        $model1->addWechatLog($uid1, 3, 't修改', 'aa_'.'bb', 'faa_'.'fbb', 'taa_'.'tbb');
+
+        dump("__________________");
+
+        dump($model1->getLogByUid(1111));
+        dump($model1-> getAllUserLog());
+        dump($model1-> getAllManagerLog());
+    
         $number = "测试======<br /> 学号 姓名 用户类型 部门 职位 注册日期 最后更新";
         $this->assign('number', $number);
         return $this->fetch();
--- application/logmanage/model/Log.php
+++ application/logmanage/model/Log.php
@@ -2,8 +2,12 @@
 /**
  * Created by 03_group.
  */
+
+namespace app\logmanage\model;
+
 use think\Model;
 use think\Db;
+use think\Request;
 
 class Log extends Model
 {
@@ -12,35 +16,66 @@ class Log extends Model
      * 杨宇 董亚聪
      * 功能：记录企业微信端的登陆和操作日志
      * @param 
-     * @uid：用户id
-     * @type ：1登陆 2增加 3修改 4删除 5查看
-     * @action：操作内容
-     * @agent ：请求头字段
-     * @ip ：对端ip
-     * @return int|string
+     * @uid：操作人的工号或学号
+     * @type: 1登陆 2增加 3修改 4删除
+     * @table ：操作的数据表名
+     * @field ：操作的数据表中的字段数组, 如果同时操作了多个字段，字段之间用下划线'_'连接
+     * @fron ：操作前的字段值，各个字段操作前的内容之间用下划线'_'连接
+     * @to ：操作后的字段值，各个字段操作后的内容之间用下划线'_'连接
+     * @return int
+     * 1. 登陆：只需要传入$uid, $type
+     * 2. 增加：只需要传入$uid, $type, $table, $field(该字段传入你增加的那条数据的主键，如果是批量添加，传入主键数组)
+     * 3. 修改：需要传入全部形参，参看形参解释
+     * 4. 删除：只需要传入$uid, $type, $table, $field(该字段传入你删除那条数据的主键，如果是批量删除，传入主键数组如['11'，'12'])
      */
- public function addWechatLog($uid, $type, $action, $agent, $ip){
-     $data = ['user_id' => $uid, 'operate_type'=> $type, 'operate_time'=> date('Y-m-d H:i:s',time()),'operate_action' => $action, 'user_agent'=> $agent,'ip' => $ip];
-     $res = Db::name('log_user')->insert($data);
-     return $res;
- }
+     public function addWechatLog($uid, $type, $table = '', $field = '', $from = '', $to =''){
+         // Db::name('user')->insertGetId($data);
+         $agent = Request::instance()->header('user-agent');
+         $ip = Request()->ip();
+         $action = [
+             'table'=> $table,
+             'field'=> $field,
+             'from'=> $from,
+             'to'=> $to
+         ];
+         dump($action);
+
+         $data = ['user_id' => $uid, 'operate_type'=> $type, 'operate_time'=> date('Y-m-d H:i:s',time()),'operate_action' => $action, 'user_agent'=> $agent,'ip' => $ip];
+         $res = Db::name('log_user')->insert($data);
+         return $res;
+     }
  
     /**
      * 苏恒杰
      * 功能：记录管理员的所有操作日志
-     * @param 
-     * @uid：用户id
-     * @type ：1登陆 2增加 3修改 4删除 5查看
-     * @action：操作内容
-     * @agent ：请求头字段
-     * @ip ：对端ip
-     * @return int|string
+     * @param
+     * @uid：操作人的管理员id
+     * @type: 1登陆 2增加 3修改 4删除
+     * @table ：操作的数据表名
+     * @field ：操作的数据表中的字段, 如果同时操作了多个字段，字段之间用下划线'_'连接
+     * @fron ：操作前的字段值，各个字段操作前的内容之间用下划线'_'连接
+     * @to ：操作后的字段值，各个字段操作后的内容之间用下划线'_'连接
+     * @return int
+     * 1. 登陆：只需要传入$uid, $type
+     * 2. 增加：只需要传入$uid, $type, $table, $field(该字段传入你增加的那条数据的主键，如果是批量添加，传入主键数组)
+     * 3. 修改：需要传入全部形参，参看形参解释
+     * 4. 删除：只需要传入$uid, $type, $table, $field(该字段传入你删除那条数据的主键，如果是批量删除，传入主键数组)
      */
- public function addMangerLog($uid,$type,$action,$agent,$ip){
-     $data = ['user_id' => $uid,'operate_type'=>$type,'operate_time'=> date('Y-m-d H:i:s',time()),'operate_action'=>$action,'user_agent'=>$agent,'ip'=>$ip];
-     $res = Db::name('log_user')->insert($data);
-     return $res;
- }
+     public function addMangerLog($uid, $type, $table = '', $field = '', $from = '', $to =''){
+         $agent = Request::instance()->header('user-agent');
+         $ip = Request()->ip();
+         $action = [
+             'table'=> $table,
+             'field'=> $field,
+             'from'=> $from,
+             'to'=> $to
+         ];
+         dump($action);
+
+         $data = ['user_id' => $uid,'operate_type'=>$type,'operate_time'=> date('Y-m-d H:i:s',time()),'operate_action'=>$action,'user_agent'=>$agent,'ip'=>$ip];
+         $res = Db::name('log_user')->insert($data);
+         return $res;
+     }
 
     /**
      * 贺文鑫
@@ -48,63 +83,42 @@ class Log extends Model
      * @param $uid
      * @return list
      */
- public function getLogByUid($uid){
-     $nameItem = Db::name('log_user')
-         ->where('user_id',$uid)
-          ->order("log_user.operate_time desc")
-         ->find();
-     return $nameItem;
- }
+     public function getLogByUid($uid){
+         $nameItem = Db::name('log_user')
+             ->where('user_id',$uid)
+              ->order("log_user.operate_time desc")
+             ->find();
+         return $nameItem;
+     }
     
     /**
-     * 链接查询 https://blog.csdn.net/weixin_42358094/article/details/81839243
      * 杨宇
      * 功能：查询所有非管理员日志
-     * @return int
+     * @return array
      */
- public function getManagerLog(){
-    $list = Db::name('log_user')->field('user_info.*, log_user.*')
-          ->join('user_info on work_id = log_user.user_id')  
-          ->where("user_info.is_delete=0")
-          ->order("log_user.operate_time desc")
-          ->select();
-     return $list;
- }
+    public function getAllUserLog(){
+        $list = Db::table('log_user')
+            ->alias('l')
+            ->join('user_info u', 'l.user_id = u.work_id')
+            ->where("u.is_delete=0")
+            ->order("l.operate_time desc")
+            ->select();
+        return $list;
+    }
 
     /**
-     * 链接查询 https://blog.csdn.net/weixin_42358094/article/details/81839243
      * 徐辉
      * 功能：查询所有管理员日志
-     * @return list
+     * @return array
      */
- public function getManagerLog(){
-    $list = Db::name('log_user')->field('manage_info.*, log_user.*')
-          ->join('manage_info on work_id = log_user.user_id')  
-          ->where("manage_info.is_delete=0")
-          ->order("log_user.operate_time desc")
-          ->select();
-     return $list;
- }
+    public function getAllManagerLog(){
+        $list = Db::table('log_user')
+            ->alias('l')
+            ->join('manage_info m', 'l.user_id = m.id')
+            ->where("m.is_delete=0")
+            ->order("l.operate_time desc")
+            ->select();
+        return $list;
+    }
     
-/* https://blog.csdn.net/luogan129/article/details/74687139
-    https://blog.csdn.net/qq_40270754/article/details/86065307
-    获取client ip
- */
-/*function get_real_ip(){ 
-    $ip=false; 
-    if(!empty($_SERVER['HTTP_CLIENT_IP'])){ 
-        $ip=$_SERVER['HTTP_CLIENT_IP']; 
-    }
-    if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){ 
-        $ips=explode (', ', $_SERVER['HTTP_X_FORWARDED_FOR']); 
-        if($ip){ array_unshift($ips, $ip); $ip=FALSE; }
-        for ($i=0; $i < count($ips); $i++){
-            if(!eregi ('^(10│172.16│192.168).', $ips[$i])){
-                $ip=$ips[$i];
-                break;
-            }
-        }
-    }
-    return ($ip ? $ip : $_SERVER['REMOTE_ADDR']); 
-}*/
 }
