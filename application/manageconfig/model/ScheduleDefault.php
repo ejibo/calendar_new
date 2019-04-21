f6349a72bcbf1c575efaf2f3eadef19aa8f72749
huangjh1995 <07f234a8735a4188aaca5d87e68088b8@huawei.com>
2019-04-21 19:06:27 +0800

    fix bug

--- application/manageconfig/model/ScheduleDefault.php
+++ application/manageconfig/model/ScheduleDefault.php
@@ -6,5 +6,13 @@ use think\model;
 
 class ScheduleDefault extends Model
 {
-
+	public function getTime(){
+        return Db::table('schedule_time')->where('id', $this->getData('time_id'))->find()['name'];
+    }
+    public function getPlace(){
+        return Db::table('schedule_place')->where('id', $this->getData('place_id'))->find()['name'];
+    }
+    public function getItem(){
+        return Db::table('schedule_item')->where('id', $this->getData('item_id'))->find()['name'];
+    }
 }
