98535ebd9d58bd937f2d3b63f72fad70289665d9
tancong <1045277922@qq.com>
2019-04-25 18:26:38 +0800

    日程查询

--- application/queryschedule/controller/Index.php
+++ application/queryschedule/controller/Index.php
@@ -1,12 +1,13 @@
 <?php
-namespace app\querySchedule\controller;
+namespace app\queryschedule\controller;
 use app\common\controller\Common as Common;
 use think\Request;
 use think\Db; //引入数据库
+use app\queryschedule\model\GetScheduleInfo as GetScheduleInfo;
 
 class Index extends Common
 {
-	protected $user_id = 1;
+	protected $user_id = 2;
 
 	protected $field_config = array(
 		array('name'=>'日期', 'field'=>'date'),
@@ -26,19 +27,26 @@ class Index extends Common
 	{
 		$starttime = $request->param('starttime');
 		$endtime = $request->param('endtime');
-		//$starttime = '2019-04-10';
-		//$endtime = '2019-05-01';
-		$sql = "select * from schedule_info where date(date) between date('".$starttime."') and date('".$endtime."')";
+
+		$sql = "select * from schedule_info
+		 where user_id=".$this->user_id." and date(date) between date('".$starttime."') and date('".$endtime."')";
 		$result = Db::query($sql);  
 		$len = count($result);
 		for($x = 0; $x < $len; $x++){
+			$time_id = $result[$x]['time_id'];
+			$time = Db::table('schedule_time')->where('id', $time_id)->value('name');
+			$result[$x]['time'] = $time;
+
 			$place_id = $result[$x]['place_id'];
 			$location = Db::table('schedule_place')->where('id', $place_id)->value('name');
 			$result[$x]['location'] = $location;
+
 			$item_id = $result[$x]['item_id'];
 			$event = Db::table('schedule_item')->where('id', $item_id)->value('name');
 			$result[$x]['event'] = $event;
 		}
+		//$mark = $starttime." ~ ".$endtime."的日程信息";
+		//$this->assign('mark', $mark);
 		$this->assign('schedule_info', $result);
 		return $this->fetch('result');
 	}
@@ -46,6 +54,20 @@ class Index extends Common
 	public function defaultList(){
 		$sql = "select * from schedule_info where user_id = ".$this->user_id." and is_delete = false";
 		$result = Db::query($sql);
+		$len = count($result);
+		for($x = 0; $x < $len; $x++){
+			$time_id = $result[$x]['time_id'];
+			$time = Db::table('schedule_time')->where('id', $time_id)->value('name');
+			$result[$x]['time'] = $time;
+
+			$place_id = $result[$x]['place_id'];
+			$location = Db::table('schedule_place')->where('id', $place_id)->value('name');
+			$result[$x]['location'] = $location;
+
+			$item_id = $result[$x]['item_id'];
+			$event = Db::table('schedule_item')->where('id', $item_id)->value('name');
+			$result[$x]['event'] = $event;
+		}
 		return $result;
 	}
 }
---
+++ application/queryschedule/model/GetScheduleInfo.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\queryschedule\model;
+
+use think\Model;
+use think\Db;
+
+class GetScheduleInfo extends Model
+{
+	public function getData()
+	{
+		return $result;
+	}
+}
