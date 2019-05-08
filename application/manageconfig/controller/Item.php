53df97cfc5c526bc359b8468c9f45e5f552a3e61
闫雅铭 <918668286@qq.com>
2019-05-08 19:02:31 +0800

    日志功能添加

--- application/manageconfig/controller/Item.php
+++ application/manageconfig/controller/Item.php
@@ -9,6 +9,8 @@
 namespace app\manageconfig\controller;
 
 use app\common\controller\Common;
+use app\logmanage\model\Log as LogModel;
+use think\Session; //引入日誌接口
 
 class Item extends Common
 {
@@ -21,12 +23,19 @@ class Item extends Common
     //添加事项的方法
     public function  addScheduleItem()
     {
+        //写入日志代码
+        $modelLog = new LogModel();
+        $uid = ADMIN_ID;
+        $type =2;
+        $table ='schedule_item';
         $des = $_POST['des'];
         $model = model('ScheduleItem');
         $isHasSame = $model->getItemByName($des);
         if ($isHasSame == null) {
             $res = $model->insertScheduleItem($des);
-            if($res ==1){
+            if($res){
+                $field =[$res];
+                $modelLog->recordLogApi($uid,$type,$table,$field);
                 $this->success("新增成功");
             }
             else{
@@ -39,10 +48,16 @@ class Item extends Common
     }
     //删除事项
     public function deleteScheduleItem(){
+        $modelLog = new LogModel();
+        $uid = ADMIN_ID;
+        $type =4;
+        $table ='schedule_item';
         $id = $_POST['id'];
+        $field=[$id];
         $model = model('ScheduleItem');
         $res = $model->deleteScheduleItem($id);
         if($res == 1){
+            $modelLog->recordLogApi($uid,$type,$table,$field);
             $this->success("删除成功");
         }
         else{
@@ -51,14 +66,21 @@ class Item extends Common
     }
     //编辑事项方法
     public function editScheduleItem(){
+        $modelLog = new LogModel();
+        $uid = ADMIN_ID;
+        $type =3;
+        $table ='schedule_item';
         $id = $_POST['id'];
         $des = $_POST['des'];
+        $yuanDes = $_POST['yuanDes'];
+        $field =[$id =>[$yuanDes,$des]];
         $model = model('ScheduleItem');
         $isSame = $model->getItemByName($des);
         if($isSame ==null){
             $res = $model->updateScheduleItem($id,$des);
             if($res ==1){
-                $this->success("编辑成功");
+                $modelLog->recordLogApi($uid,$type,$table,$field);
+                $this->success("编辑成功!");
             }
             else{
                 $this->error(  "编辑失败，请重新操作!");
@@ -67,5 +89,15 @@ class Item extends Common
         else{
             $this->error(  "修改事项名称与已有重复，请重新修改!");
         }
+    }
+    function unicodeDecode($data)
+    {
+        function replace_unicode_escape_sequence($match) {
+            return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+        }
+
+        $rs = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', 'replace_unicode_escape_sequence', $data);
+
+        return $rs;
     }
 }
