7ac448fd9294b3e617c3d93fc161acb755f7ab0c
Huangjh <516585452@qq.com>
2019-05-10 17:56:37 +0800

    修复bug，优化代码

--- application/manageconfig/controller/Calendar.php
+++ application/manageconfig/controller/Calendar.php
@@ -11,6 +11,7 @@ namespace app\manageconfig\controller;
 
 use app\common\controller\Common;
 use app\manageconfig\model\ScheduleDefault;
+use http\Exception\InvalidArgumentException;
 use think\Db;
 use think\Request;
 
@@ -22,10 +23,7 @@ class Calendar extends Common
             $username="张三";//测试
         }
         $this->assign("username",$username);
-        $user_id = Db::table("user_info")->where(["name" => $username, "is_delete" => 0])->find()['id'];
-        $defaultSchedule=model('ScheduleDefault');//new ScheduleDefault();
-        $defaultSchedules=$defaultSchedule->where('user_id',$user_id)-> select();
-        $this->assign('defaultSchedules',$defaultSchedules);
+        $this->assign('defaultSchedules',ScheduleDefault::getDefaultSchedules($username));
         return $this->fetch();
     }
     /**
@@ -34,50 +32,26 @@ class Calendar extends Common
     public function addDefaultSchedule()
     {
         $param = Request::instance()->post();
-        $place = $param['place'];
-        $item = $param['item'];
-        $time = $param['time'];
         $this->validate($param,'ScheduleDefault');
         $username = session('username');
         if(empty($username)){
             $username="张三";//测试
         }
-        $user_id = Db::table("user_info")->where(["name" => $username, "is_delete" => 0])->find()['id'];
-        if (empty($user_id)) {
-            return json(["code" => 400, 'msg' => '用户['.$username.']不存在', 'data' => []]);
-        }
-        $time_id = Db::table('schedule_time')->where('name', $time)->find()['id'];
-        if (empty($time_id)) {
-            return json(['code' => 1, 'msg' => '未定义的时间段', 'data' => []]);
-        }
-        $res = Db::table('schedule_default')->where('user_id', $user_id)->where('time_id', $time_id)->where('is_delete', 0)->find();
-        if ($res != null){
-            return json(['code' => 2, 'msg' => '已存在该时间段的默认日程，可点击编辑进行修改', 'data' => []]);//时间必须是之前配置好的时间
-        }
-        $place_id=Db::table('schedule_place')->where('name',$place)->find()['id'];
-        if(empty($place_id)){
-            $place_id=Db::table('schedule_place')->insertGetId(['name'=>$place,'is_delete'=>0]);//如果是之前不存在的地点，则新建一个
-        }
-        $item_id=Db::table('schedule_item')->where('name',$item)->find()['id'];
-        if(empty($item_id)){
-            $item_id=Db::table('schedule_item')->insertGetId(['name'=>$item,'is_delete'=>0]);//如果是之前不存在的事项，则新建一个
-        }
-        //开始检查之前是否已经有一样的默认日程了
         $schedule=model('ScheduleDefault');
-        $schedule->user_id=$user_id;
-        $schedule->time_id=$time_id;
-        $schedule->place_id=$place_id;
-        $schedule->item_id=$item_id;
-        $schedule->is_delete=0;
-        if($schedule->find($schedule->getData())!=null){
-            return json(['code'=>3,'msg'=>'已存在相同的记录','data'=>[]]);
+        try{
+            $schedule->setUser($username);
+            $schedule->setTime($param['time']);
+        }catch(InvalidArgumentException $e) {
+            return json(['code'=>$e->getCode(),'msg'=>$e->getMessage(),'data'=>[]]);
         }
+        $schedule->setPlace($param['place']);
+        $schedule->setItem($param['item']);
+        $schedule->is_delete=0;
         if($schedule->save()){
-            return $this->success('操作成功', url('index'));
+            return json(['code'=>1,'msg'=>'success','data'=>[]]);
         }else{
             return json(['code'=>-1,'msg'=>'添加失败，发生未知错误','data'=>[]]);
         }
-
     }
   
   	/**
