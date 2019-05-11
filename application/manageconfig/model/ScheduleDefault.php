a21dae3bccfa02583b7425a435c3c5d6e5397aa2
Huangjh <516585452@qq.com>
2019-05-11 16:41:37 +0800

    。。。

--- application/manageconfig/model/ScheduleDefault.php
+++ application/manageconfig/model/ScheduleDefault.php
@@ -9,16 +9,19 @@ class ScheduleDefault extends Model
 {
     public static function getDefaultSchedules($username){
         $user_id = Db::table("user_info")->where(["name" => $username, "is_delete" => 0])->find()['id'];
-        $defaultSchedule=model('ScheduleDefault');//new ScheduleDefault();
+        $defaultSchedule=new ScheduleDefault();
         $defaultSchedules=$defaultSchedule->where(['user_id'=>$user_id, "is_delete" => 0])-> select();
         return $defaultSchedules;
     }
+
     public function getTime(){
         return Db::table('schedule_time')->where('id', $this->getData('time_id'))->find()['name'];
     }
+
     public function getPlace(){
         return Db::table('schedule_place')->where('id', $this->getData('place_id'))->find()['name'];
     }
+
     public function getItem(){
         return Db::table('schedule_item')->where('id', $this->getData('item_id'))->find()['name'];
     }
@@ -30,18 +33,20 @@ class ScheduleDefault extends Model
         }
         $this->data('user_id',$user['id']);
     }
+
     /**
      * 时间必须是之前配置好的时间，正常来说传过来的时间都是已经存在了的。<br>
      * 并检查之前是否已经有一样的默认日程了
      */
     public function setTime($time){
-        $time_id = Db::table('schedule_time')->where(['name'=> $time, "is_delete" => 0])->find()['id'];
+        $time_id = Db::table('schedule_time')->where('name', $time)->find()['id'];
         if (empty($time_id)) {//时间必须是之前配置好的时间，正常来说传过来的时间都是已经存在了的
             throw new \InvalidArgumentException($time.'是未定义的时间段',404);
         }
         $this->data('time_id',$time_id);
         $this->checkSameTimeDefaultSchedule();
     }
+
     /**
      * 检查之前是否已经有一样的默认日程了
      */
@@ -52,6 +57,7 @@ class ScheduleDefault extends Model
             throw new \InvalidArgumentException('已存在该时间段的默认日程，可点击编辑进行修改,创建时间：'.$res['create_time']);
         }
     }
+
     public function setPlace($place){
         $place_id=Db::table('schedule_place')->where(['name'=>$place,'is_delete'=> 0])->find()['id'];
         if(empty($place_id)){//如果是之前不存在的地点，则新建一个
@@ -59,6 +65,7 @@ class ScheduleDefault extends Model
         }
         $this->data('place_id',$place_id);
     }
+
     public function setItem($item){
         $item_id=Db::table('schedule_item')->where(['name'=>$item,'is_delete'=> 0])->find()['id'];
         if(empty($item_id)){//如果是之前不存在的事项，则新建一个
