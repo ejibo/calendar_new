c894a62b94a505091258db9a9ad6aad10624e19b
Hu Yingcong <huyc0769@qq.com>
2019-05-14 19:25:13 +0800

    wx_calendar.html修改

--- application/wxcampus/controller/WxCalendar.php
+++ application/wxcampus/controller/WxCalendar.php
@@ -6,12 +6,14 @@ use think\Controller;
 use app\wx\controller\Personal;
 class WxCalendar extends Personal
 {
-    protected $cells;
     public function Index(){
-        $this->assign($cells, $this->getOneDaySchedule(strtotime('2019-04-08')));
+        $this->assign('date', date('Y-m-d'));
+        $this->assign('cells', $this->getOneDaySchedule(strtotime('2019-04-09')));
         return $this->fetch("index/wx_calendar");
     }
-    public function click(){
+    public function add(){
+        $this->assign('schedule_item', $this->getScheduleItem());
+        $this->assign('schedule_place', )
         return $this->fetch("index/wx_add");
     }
 }
