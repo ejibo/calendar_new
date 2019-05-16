d819d398353d55141a8116885509e7631331ca64
heresjen <7aeee770a0e441bf9277376b5b2caadc@huawei.com>
2019-05-16 16:58:52 +0800

    ss

--- application/wxcampus/controller/Wxquery.php
+++ application/wxcampus/controller/Wxquery.php
@@ -20,18 +20,18 @@ class Wxquery extends controller
         //$name = Request::instance()->param('query_name');
         //$work_id = Request::instance()->param('query_name');
       
-        $res = Db::table('schedule_info')
-            ->alias(['schedule_info' => 'a', 'user_info' => 'b', 'user_position' => 'c', 'schedule_time' => 'd', 'schedule_place' => 'e', 'schedule_item' => 'f'])
-            ->where('a.is_delete', 0)
-            ->where('b.name', $query_name)
-            ->where('b.work_id', $work_id)
-            ->join('b', 'a.user_id = b.id')
-            ->join('c', 'b.position_id = c.id')
-            ->join('d', 'a.time_id = d.id')
-            ->join('e', 'a.place_id = e.id')
-            ->join('f', 'a.item_id = f.id')
-            ->field('b.name as name, d.name as time, e.name as place, f.name as item, b.id as id')
-            ->select();
+        // $res = Db::table('schedule_info')
+        //     ->alias(['schedule_info' => 'a', 'user_info' => 'b', 'user_position' => 'c', 'schedule_time' => 'd', 'schedule_place' => 'e', 'schedule_item' => 'f'])
+        //     ->where('a.is_delete', 0)
+        //     ->where('b.name', $query_name)
+        //     ->where('b.work_id', $work_id)
+        //     ->join('b', 'a.user_id = b.id')
+        //     ->join('c', 'b.position_id = c.id')
+        //     ->join('d', 'a.time_id = d.id')
+        //     ->join('e', 'a.place_id = e.id')
+        //     ->join('f', 'a.item_id = f.id')
+        //     ->field('b.name as name, d.name as time, e.name as place, f.name as item, b.id as id')
+        //     ->select();
 
         // $this->assign('res',$res);
         // return $this->fetch();
