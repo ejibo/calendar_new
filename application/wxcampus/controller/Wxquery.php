3241a6ad141a2e8d493c5d3026120ea9cd4a5625
heresjen <7aeee770a0e441bf9277376b5b2caadc@huawei.com>
2019-05-16 10:06:35 +0800

    修改fetch链接

--- application/wxcampus/controller/Wxquery.php
+++ application/wxcampus/controller/Wxquery.php
@@ -32,6 +32,6 @@ class Wxquery extends controller
             ->select();
 
         $this->assign('res',$res);
-        return $this->fetch();
+        return $this->fetch("index/wx_search");
     }
 }
