2dac715310a01d5c56672f2a1cb0c03d97f7cb8c
heresjen <7aeee770a0e441bf9277376b5b2caadc@huawei.com>
2019-05-18 19:18:15 +0800

    ss

--- application/wxcampus/controller/Wxquery.php
+++ application/wxcampus/controller/Wxquery.php
@@ -24,8 +24,9 @@ class Wxquery extends controller
         $list = $query->wx_query(); // 使用模型中的wx_query方法
         // dump($res);
 
-        $this->assign('list', $query_name);
-        return $this->fetch('index/wx_search');
+        //$this->assign('list', $query_name);
+        //return $this->fetch('index/wx_search');
+    echo $query_name;
     }
   
   	public function wxquery()
