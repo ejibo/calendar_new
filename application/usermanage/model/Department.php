7f8b3a1a80773e221fc698f75c2652a2bcca4d89
Anastasia-Lu <fcc4523bc0294fa2b70af3b758bd1d88@huawei.com>
2019-05-18 16:12:16 +0800

    修改部门名称判断规则

--- application/usermanage/model/Department.php
+++ application/usermanage/model/Department.php
@@ -80,7 +80,7 @@ class Department extends Model
           $message = '部门名称不能为空';
     	return ['message' => $message];
 		}
-      if(preg_match('^[\u4E00-\u9FA5A-Za-z0-9]+/u',$name)){
+      if(!preg_match("/^[\x{4e00}-\x{9fa5}A-Za-z0-9_]+$/u",$name)){
       如果输入的部门名称中包含标点符号
          $message = '部门名称不能包含标点符号';
     	return ['message' => $message];
