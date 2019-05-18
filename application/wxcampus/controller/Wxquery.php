4dca98a5e142b60aad7cdc8f453e81f0353c4fa5
heresjen <7aeee770a0e441bf9277376b5b2caadc@huawei.com>
2019-05-18 15:54:19 +0800

    ss

--- application/wxcampus/controller/Wxquery.php
+++ application/wxcampus/controller/Wxquery.php
@@ -18,22 +18,21 @@ class Wxquery extends controller
     public function Index()
     {
     	//按照部门、职务、姓名 查询用户日程
-    	//$query = new query();  // 实例化模型
-        //$list = $query->wx_query(); // 使用模型中的wx_query方法
-        //var_dump($list);
-      
-        // echo $list;
-        $this->assign('list', 'hello');
-        return $this->fetch('index/wx_search');
-    }
-  
-  	public function query()
-    {
-        $query = new query();  // 实例化模型
+    	$query = new query();  // 实例化模型
         $list = $query->wx_query(); // 使用模型中的wx_query方法
         // dump($res);
 
         $this->assign('list', $list);
         return $this->fetch('index/wx_search');
+    }
+  
+  	public function query()
+    {
+        //$query = new query();  // 实例化模型
+        //$list = $query->wx_query(); // 使用模型中的wx_query方法
+        // dump($res);
+
+        //$this->assign('list', $list);
+        // return $this->fetch('index/wx_search');
     }
 }
