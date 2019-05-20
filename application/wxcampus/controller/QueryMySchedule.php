6fe4a98ac25ea9f5e41f389a5345d1895355ea50
George <1045277922@qq.com>
2019-05-20 22:46:34 +0800

    x

--- application/wxcampus/controller/QueryMySchedule.php
+++ application/wxcampus/controller/QueryMySchedule.php
@@ -61,6 +61,22 @@ class QueryMySchedule extends Controller
 		}
 	}
 
+	public function index0()
+	{
+		$number = Request::instance()->param('number');
+		$result = $this->defaultList($number);
+		if($result == NULL){
+			echo "没有您的日程信息";
+		}
+		else{
+			$this->assign('date', date('Y-m-d'));
+			$this->assign('user_id', $this->user_id);
+			$this->assign('schedule_info', $result);
+			$this->assign('fields', $this->field_config);
+			return $this->fetch('index0');
+		}
+	}
+
 	public function getMyScheduleInfo(Request $request)
 	{
 		$starttime = $request->param('starttime');
