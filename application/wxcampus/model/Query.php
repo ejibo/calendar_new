333069016c496872a55eec50bdc12c2220339d42
heresjen <7aeee770a0e441bf9277376b5b2caadc@huawei.com>
2019-05-22 21:46:08 +0800

    ss

--- application/wxcampus/model/Query.php
+++ application/wxcampus/model/Query.php
@@ -12,8 +12,8 @@ class Query extends Model
     {
         $query_depart = input('post.select1');
         $query_pos = input('post.select2');
-        $query_name = input('post.query_name');
-        $work_id = input('post.work_id');
+        $query_name = Request::instance()->post('query_name','','strip_tags,htmlspecialchars');
+        $work_id = Request::instance()->post('work_id','','strip_tags,htmlspecialchars');
 
         
         $allInfo = array();
