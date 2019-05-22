bdb08bbd808bbb26a3d79b5f756a6f73f894492b
heresjen <7aeee770a0e441bf9277376b5b2caadc@huawei.com>
2019-05-22 16:13:22 +0800

    限制日期，当日之后

--- application/wxcampus/model/Query.php
+++ application/wxcampus/model/Query.php
@@ -9,28 +9,31 @@ use think\Request;
 class Query extends Model
 {
     public function wx_query()
-    {        
+    {
+        $query_depart = input('post.select1');
+        $query_pos = input('post.select2');
         $query_name = input('post.query_name');
         $work_id = input('post.work_id');
 
-        // $query_name = Request::instance()->param('query_name');
-        // $work_id = Request::instance()->param('query_id');
-
-        // $date1 = new DateTime("now");
+        
         $allInfo = array();
         $allInfo = Db::table('schedule_info')
             // ->alias(['schedule_info' => 'a', 'user_info' => 'b', 'user_position' => 'c', 'schedule_time' => 'd', 'schedule_place' => 'e', 'schedule_item' => 'f'])
             ->join('user_info', 'schedule_info.user_id = user_info.id')
-            // ->join('user_position', 'schedule_info.position_id = user_position.id')
+            ->join('user_depart', 'schedule_info.depart_id = user_depart.id')
+            ->join('user_position', 'schedule_info.position_id = user_position.id')
             ->join('schedule_time', 'schedule_info.time_id = schedule_time.id')
             ->join('schedule_place', 'schedule_info.place_id = schedule_place.id')
             ->join('schedule_item', 'schedule_info.item_id = schedule_item.id')
             ->where('schedule_info.is_delete', 0)
+            ->where('user_info.is_delete', 0)
+            ->where('user_info.is_delete', 0)
+            ->where('user_depart.name', $query_depart)
+            ->where('user_position.name', $query_pos)
             ->where('user_info.name', $query_name)
             ->where('user_info.work_id', $work_id)
-            // ->where(date_diff('2018-09-09', '2018-10-09') > 0)
+            ->where('date', '>= time', date('Y-m-d', time()))
             ->field('user_info.name as name, schedule_time.name as time, schedule_place.name as place, schedule_item.name as item, user_info.id as id')
-            // ->field('id, user_id')
             ->select();
 
         
@@ -38,13 +41,7 @@ class Query extends Model
             // return $this->index();
             echo '输入不能为空';
         }else{
-            // // 遍历数据集
-            foreach($allInfo as $schedule){
-                $schedule['id'] = isset($schedule['id'])?$schedule['id']:"1";
-                $schedule['user_id'] = isset($schedule['user_id'])?$schedule['user_id']:"1";
-            
-                return $schedule;
-            }
+            return $allInfo;            
         }
       
     }
