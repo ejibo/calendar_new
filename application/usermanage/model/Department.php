26a058093963793d409389f3ef6ab1c1e94fadc0
Anastasia-Lu <fcc4523bc0294fa2b70af3b758bd1d88@huawei.com>
2019-05-24 23:01:24 +0800

    $message = '部门名称太长';

--- application/usermanage/model/Department.php
+++ application/usermanage/model/Department.php
@@ -82,7 +82,7 @@ class Department extends Model
 		}
       if(strlen($name) > 30){
       //如果部门名称大于25个字符
-        $message = '部门名称太长;
+        $message = '部门名称太长';
         return ['message' => $message];
       }
       if(!preg_match('/^[\x{4e00}-\x{9fa5}A-Za-z0-9_]+$/u',$name)){
