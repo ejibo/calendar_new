b77082c296a828582d20ba5134eed1163b698df8
junzhiL <junzhi@pku.edu.cn>
2019-05-25 19:29:01 +0800

    更新添加whitelist

--- application/manageconfig/model/Whitelist.php
+++ application/manageconfig/model/Whitelist.php
@@ -13,14 +13,15 @@ use think\Db;
 class Whitelist extends Model{
     public function getinfo(){
         //页面table初始化，得到所有白名单人员的信息
-        $info = Db::table('user_info')
-            ->alias(['user_info' => 'ui', 'user_depart' => 'ud', 'user_position' => 'up'])
+        $info = Db::table('white_list')
+            ->alias(['white_list' => 'ui', 'user_depart' => 'ud', 'user_position' => 'up'])
             ->where('ui.is_delete',0)
             ->where('ud.is_delete',0)
             ->where('up.is_delete',0)
             ->join('user_depart','ui.depart_id = ud.id')
             ->join('user_position','ui.position_id = up.id')
             ->field('ui.id,ui.name as ui_name,ui.work_id,ui.type_id,ui.depart_id,ui.position_id,ud.name as ud_name,up.name as up_name')
+            ->order('create_time')
             ->select();
         return $info;
     }
@@ -49,6 +50,33 @@ class Whitelist extends Model{
         $is_delete = Db::table('user_info')->where('id',$data['del_id'])
             ->update(['is_delete' => 1]);
         return $is_delete;
+    }
+    //根据depart_id判断部门是否存在
+    public function exist_depart($depart_id){
+        $isexist = Db::table('user_depart')->where('id',$depart_id)->where('is_delete',0)->find();
+        if ($isexist==null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    //根据position_id判断职位是否存在
+    public function exist_position($position_id){
+        $isexist = Db::table('user_position')->where('id',$position_id)->where('is_delete',0)->find();
+        if ($isexist==null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    //判断工号是否已存在且有效
+    public function exist_work_id($work_id){
+        $isexist = Db::table('white_list')->where('work_id',$work_id)->where('is_delete',0)->find();
+        if ($isexist==null){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     /*
