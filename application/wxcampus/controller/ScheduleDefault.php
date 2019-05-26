e883eaf189d595dddaeef39e4bee1d64cd85ef71
lyl <lyl0724@pku.edu.cn>
2019-05-26 15:14:00 +0800

    fix bug

--- application/wxcampus/controller/ScheduleDefault.php
+++ application/wxcampus/controller/ScheduleDefault.php
@@ -86,12 +86,11 @@ class ScheduleDefault extends Controller
       	$this->assign("place_id", $place_id);
         return $this->fetch();
     }
-    public function updateDefaultSchedule($uid, $wxcode)
+    public function updateDefaultSchedule($uid, $wxcode, $place, $item, $id)
     {
-        $param = Request::instance()->post();
-        $place = $param['place'];
-        $item = $item['item'];
-        $id = $param['id'];
+        $place = $place;
+        $item = $item;
+        $id = $id;
 
         $place_id=Db::table('schedule_place')->where('name',$place)->find()['id'];
         $item_id=Db::table('schedule_item')->where('name',$item)->find()['id'];
